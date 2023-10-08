d8451daf1b970051820d0fe1274b4c83199a5fd3
erviti88 <ignacio.erviti@bootcamp-upgrade.com>
2023-10-08 13:10:40 +0200

    Components project created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RoomComponent } from './components/room/room.component';
+import { MazeComponent } from './components/maze/maze.component';
+import { ControlsComponent } from './components/controls/controls.component';
+import { PlayerComponent } from './components/player/player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomComponent,
+    MazeComponent,
+    ControlsComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
